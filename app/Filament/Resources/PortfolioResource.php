293d13f622cd9923cc031f74a0d6bc62b46f73b0
reuben <reuben154.infinity@gmail.com>
2024-05-30 17:07:19 +0700

    portfolio resource

---
+++ app/Filament/Resources/PortfolioResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PortfolioResource\Pages;
+use App\Models\Portfolio;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class PortfolioResource extends Resource
+{
+    protected static ?string $model = Portfolio::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-code-bracket';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()
+                ->schema([
+                    Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('subtitle')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('link')
+                    ->maxLength(255)
+                    ->default(null),
+                    Forms\Components\RichEditor::make('content')
+                    ->columnSpanFull(),
+                ])
+                ->columns(3)
+                ->columnSpan(2),
+                Forms\Components\Section::make()
+                ->schema([
+                    Forms\Components\FileUpload::make('image')
+                        ->image(),
+                    Forms\Components\TagsInput::make('technologies'),
+                ])
+                ->columnSpan(1),
+            ])
+            ->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subtitle')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('link')
+                    ->searchable(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPortfolios::route('/'),
+            'create' => Pages\CreatePortfolio::route('/create'),
+            'edit' => Pages\EditPortfolio::route('/{record}/edit'),
+        ];
+    }
+}
