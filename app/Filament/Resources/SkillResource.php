3d82ad1c6eb1208c018da60e5b34a93b00368e63
reuben <reuben154.infinity@gmail.com>
2024-05-30 17:06:08 +0700

    skills management

---
+++ app/Filament/Resources/SkillResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SkillResource\Pages;
+use App\Models\Skill;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class SkillResource extends Resource
+{
+    protected static ?string $model = Skill::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('icon')
+                    ->image()
+                    ->default(null),
+                Forms\Components\TextInput::make('percentage')
+                    ->numeric()
+                    ->default(null),
+            ])
+            ->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('icon')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('percentage')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSkills::route('/'),
+        ];
+    }
+}
