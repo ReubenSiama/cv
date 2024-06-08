9ce7153548b782a8745141c91b92c816a1fa402a
ReubenSiama <reubenlalhmunsiama@gmail.com>
2024-06-09 02:44:43 +0700

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ export default {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#151515',
+        'light-black': '#18181B',
+        'pencil': '#353839',
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
