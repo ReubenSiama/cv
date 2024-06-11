21742c1b90af3571eb6fa49dd3d77094f23ee2fc
reuben <reuben154.infinity@gmail.com>
2024-06-11 17:59:12 +0700

    portfolio page redesign

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -15,9 +15,20 @@
             <span class="bg-gray-700 text-white px-2 py-1 rounded-lg mr-2">{{ $technology }}</span>
         @endforeach
     </div>
-    <img src="{{ asset('storage/'.$portfolio->image) }}" alt="{{ $portfolio->title }}" class="object-cover w-full rounded-lg">
     <div class="p-4 bg-light-black rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        {!! $portfolio->content !!}
+        <div id="portfolio-content">
+            <img src="{{ asset('storage/'.$portfolio->image) }}" alt="{{ $portfolio->title }}"
+            class="object-cover w-full md:w-[30%] mr-6 rounded-lg md:float-start">
+            {!! $portfolio->content !!}
+        </div>
     </div>
 </div>
+
+<style>
+    #portfolio-content li{
+        list-style-type: disc;
+        list-style-position: inside;
+        padding: 5px;
+    }
+</style>
 @endsection
