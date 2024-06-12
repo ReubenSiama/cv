58537b985ab10aad4b73e2bf10e318f5fea59adf
reuben <reuben154.infinity@gmail.com>
2024-06-12 16:53:56 +0700

    remove space

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reuben Siama {{ ' - '.$title }}</title>
+    <title>Reuben Siama {{ '- '.$title }}</title>
     <meta name="description" content="{{ $description ?? 'Reuben Siama\'s personal website'}}">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
