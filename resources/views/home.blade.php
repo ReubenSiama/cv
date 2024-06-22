79fd13966a8ed50fc763454f91f87d26b15f3c2b
reuben <reuben154.infinity@gmail.com>
2024-06-22 08:35:50 +0700

    add s

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
             <br>
             <div class="md:flex md:content-center items-center grid text-center gap-4">
                 <a href="{{ route('portfolios') }}" class="px-4 py-2 bg-green-500 text-white rounded-lg mr-4 hover:bg-green-400">
-                    Explore my work
+                    Explore my works
                 </a>
                 <a href="{{ route('experiences')}}" class="underline hover:text-gray-300">
                     Dive into my career timeline and milestones.
