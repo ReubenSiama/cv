991ae1bdec08f918723b78a87e3d6e4ba5eb6023
reuben <reuben154.infinity@gmail.com>
2024-06-24 10:09:50 +0700

    maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reuben Siama - Maintenance Mode</title>
+    <meta name="description" content="Reuben Siama\'s personal website">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
+</head>
+<body class="h-screen bg-black text-white items-center content-center">
+    <div class="text-center font-extralight text-4xl">
+        Dear visitor, this site is currently undergoing maintenance.
+        <br>
+        Please check back later. Thank you for your patience.
+    </div>
+    <div class="text-center font-extralight mt-10">
+        "Change is the law of life. And those who look only to the past or present are certain to miss the future." - John F. Kennedy
+    </div>
+</body>
+</html>
