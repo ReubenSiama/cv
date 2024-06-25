0d17124372f857ff23285a7dae3cfaa748f17825
reuben <reuben154.infinity@gmail.com>
2024-06-25 12:24:29 +0700

    error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,14 +3,6 @@
     'class' => 'items-center content-center'
     ])
 @section('body')
-    <div class="mx-auto items-center content-center justify-center">
-        <x-filament::icon
-            alias="panels::topbar.global-search.field"
-            icon="heroicon-m-lock-closed"
-            wire:target="search"
-            class="h-10 w-10 text-gray-500 dark:text-gray-400 text-center m-auto"
-        />
-    </div>
     <div class="text-center font-extralight text-2xl">
         Can't let you in.
         <br>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,12 +4,14 @@
     ])
 @section('body')
     <div class="text-center font-extrabold text-9xl">
-        5<span class="text text-red-500">0</span>3
+        4<span class="text text-red-500">0</span>4
     </div>
     <div class="text-center font-extralight text-2xl">
-        We are under maintenance
+        Oops! It looks like you've stumbled upon a page that doesn't exist.
+        <br>
+        Don't worry, it happens to the best of us.
     </div>
     <div class="text-center italic">
-        Please check back later.
+        Visit <a href="{{ route('home') }}" class="text-red-500">homepage</a> to explore more.
     </div>
 @endsection
