83027547a1fa02f6aec9239e74c6ff3f3c1952b7
reuben <reuben154.infinity@gmail.com>
2024-11-15 15:03:06 +0700

    middlware group

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,23 @@ use App\Http\Middleware\VisitorCountMiddleware;
 use App\Livewire\Calculation;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(HomeController::class)
-    ->middleware(VisitorCountMiddleware::class)
+Route::middleware(VisitorCountMiddleware::class)
     ->group(function () {
-        Route::get('/', 'index')->name('home');
-        Route::get('/contact', 'contact')->name('contact');
-        Route::post('/contact', 'storeContact')->name('contact.store');
-        Route::get('/portfolios', 'portfolios')->name('portfolios');
-        Route::get('/portfolios/{portfolio}', 'view')->name('portfolios.view');
-        Route::get('/experiences', 'experiences')->name('experiences');
+        Route::controller(HomeController::class)
+            ->group(function () {
+                Route::get('/', 'index')->name('home');
+                Route::get('/contact', 'contact')->name('contact');
+                Route::post('/contact', 'storeContact')->name('contact.store');
+                Route::get('/portfolios', 'portfolios')->name('portfolios');
+                Route::get('/portfolios/{portfolio}', 'view')->name('portfolios.view');
+                Route::get('/experiences', 'experiences')->name('experiences');
+            });
+
+        Route::controller(BlogPostController::class)
+            ->group(function () {
+                Route::get('/blog', 'index')->name('blog.index');
+                Route::get('/blog/{blogPost}', 'view')->name('blog.view');
+            });
     });
 
 Route::get('/calculate', Calculation::class)->name('calculate');
-Route::controller(BlogPostController::class)
-    ->group(function () {
-        Route::get('/blog', 'index')->name('blog.index');
-        Route::get('/blog/{blogPost}', 'view')->name('blog.view');
-    });
