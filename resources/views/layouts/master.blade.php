a664fb66b14b1e63e3d6feee80a8497d71dc5071
reuben <reuben154.infinity@gmail.com>
2024-11-18 12:29:28 +0700

    bug

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reuben Siama {{ '- '.$title }}</title>
     <meta name="description" content="{{ $description ?? 'Reuben Siama\'s personal website'}}">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/prism.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('css/prism.css')}}">
 </head>
 <body class="bg-black text-white {{ $class ?? ''}}">
     @yield('body')
+    <script src="{{ asset('js/prism.js') }}"></script>
 </body>
 </html>
